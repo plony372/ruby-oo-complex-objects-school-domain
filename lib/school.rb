3c2e63afbfaf2716b8eee16f71101380a114ecc2
MenachemFuterfas <menachemfuterfas@gmail.com>
2021-02-24 15:59:11 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+class School
+    attr_accessor :name, :roster, :add_student, :grade, :sort
+
+    def initialize(name=nil, roster)
+        @name = name
+        @roster = {}
+    end 
+
+    def add_student(studentName, grade)
+
+        if @roster[grade]
+            @roster[grade].push(studentName)
+        else
+          @roster[grade] = []
+          @roster[grade].push(studentName)
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        new = @roster.map {|k, v | v.sort!}
+        @roster
+        # new.map {|i| }
+        # @roster.map {|k| p k}
+        
+    end
+ 
+end
+
+school = School.new("Bayside High School")
+school.add_student('getzel', 9)
+school.add_student("frank", 9)
+school.add_student('getzel', 9)
+school.add_student("frank", 10)
+p school.sort
